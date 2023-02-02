4d6aead77f653dfc6559fcb8381b478720226c14
Phạm Tiến Duy <86275419+duypt02@users.noreply.github.com>
2023-02-02 16:38:28 +0700

    Add files via upload

---
+++ web_vuln/view/homepage.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!-- Created By CodingNepal - www.codingnepalweb.com -->
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Home Page</title>
+    <link rel="stylesheet" href="view/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+</head>
+<body>
+<div class="img"></div>
+<div class="center">
+    <h2 class="title">
+        <span class="title-word title-word-1">Welcome</span>
+        <span class="title-word title-word-2">to</span>
+        <span class="title-word title-word-3">Duy.vn</span>
+    </h2>
+    <div class="btns">
+        <button><a href="?action=signin" class="login">Đăng nhập</a></button>
+        <button><a href="?action=signup" class="signin">Đăng ký</a></button>
+    </div>
+</div>
+</body>
+</html>
